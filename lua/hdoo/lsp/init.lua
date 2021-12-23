b9bd76fd033f8de153cb526edf51e0195f4ba9c4
Yui <tyeirdoo06@gmail.com>
2021-12-24 04:36:20 +0900

    hidden

---
+++ lua/hdoo/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "hdoo.lsp.lsp-installer"
+require("hdoo.lsp.handlers").setup()
+require "hdoo.lsp.null-ls"
