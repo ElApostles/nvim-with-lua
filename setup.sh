066a2766eddc7dd54547abda7089ff5e5d50c663
Hyeokjin Doo <hdoo@c3r2s1.42seoul.kr>
2022-03-09 10:55:39 +0900

    add source bashrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "alias vim='~/.config/nvim/nvim.appimage'" > ~/.bashrc
+source ~/.bashrc
 cd ~/.config/nvim/
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage --output nvim.appimage
 chmod u+x nvim.appimage
